ca390f3974a5d73ef2a9fafe056eb0a077c58360
Lyle Underwood <lyle.underwood@bkwld.com>
2011-04-09 21:56:32 -0700

    Added README.rdoc

--- lib/schlepp.rb
+++ lib/schlepp.rb
@@ -1,3 +1,9 @@
+# schlepp makes it easy to normalize data from various sources like CSV. Need 
+# to go from flattened CSV to relational tables? Schlepp provides a 
+# simple DSL for mapping, grouping and importing data from and to various 
+# formats.
+#
+# At least that's the plan. Currently handles access to CSV files.
 require 'schlepp/burden'
 
 module Schlepp
