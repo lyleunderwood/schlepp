9b3f577ddce936ea017e319c1f5c1e55ca1375d8
Lyle Underwood <lyle.underwood@bkwld.com>
2011-04-09 20:16:08 -0700

    Refactored some stuff, added some documentation

--- lib/schlepp/format/base.rb
+++ lib/schlepp/format/base.rb
@@ -1,4 +1,6 @@
 module Schlepp
+  # a Format is a class which handles reading, parsing, mapping, and grouping a
+  # file type (like CSV).
   module Format
     @cwd = ''
 
@@ -8,14 +10,22 @@ module Schlepp
 
     class Base
 
-      attr_accessor :name, :required
+      # the filename
+      # TODO: support filename glob like format.name = :glob => 'data*.csv'
+      attr_accessor :name
 
+      # defaults to false
+      attr_accessor :required
+
+      # takes a config block which receives self as parameter
       def initialize(&block)
         self.required = false
 
         block.call(self)
       end
 
+      # read the file. uses +cwd+ set by Schlepp::Burden#cd. Returns nil if no
+      # file is found.
       def read
         begin
           path = Format.cwd == '' ? name : File.join(Format.cwd, name)
@@ -25,14 +35,18 @@ module Schlepp
         end
       end
 
+      # block to run before file is read
       def before(&block)
         @before = block
       end
 
+      # block to run after file is processed
       def after(&block)
         @after = block
       end
 
+      # throws an error if a required file is not found. runs our before,
+      # then process_file, then our after.
       def process!
         data = read
         if data.nil?
@@ -41,12 +55,12 @@ module Schlepp
         end
 
         @before.call(data) if @before
-
         result = process_file(data)
-
         @after.call(result) if @after
       end
 
+      # should be implemented by subclasses. handles parsing, mapping, and
+      # grouping. returns the grouped data.
       def process_file(data)
         raise "Implement process_file in Schlepp::Format::Base #{__FILE__}"
       end
--- lib/schlepp/format/csv.rb
+++ lib/schlepp/format/csv.rb
@@ -3,53 +3,78 @@ require 'csv'
 
 module Schlepp
   module Format
+    # Uses CSV from the standard library to parse a CSV file. Handles mapping
+    # CSV lines to field names and nesting data based on grouping columns.
     class Csv < Base
 
+      # This is literally just a hash which can have children, accessible from
+      # #children.
       class Item < Hash
+        # child Item objects
         attr_accessor :children
 
-        def initialize(*opts)
+        def initialize(*opts) # :nodoc:
           @children = []
           super
         end
       end
 
-      attr_accessor :mapping, :groups
+      attr_accessor :mapping # :nodoc:
 
+      # should be an array of column numbers to group on, ex: [3, 6, 4] in
+      # order of trunk to leaves
+      attr_accessor :groups
+
+      # takes a config block which gets run through +instance_eval+.
       def initialize(&block)
 
         super
       end
 
+      # load our mapping YAML config into #mapping.
       def load_mapping(path)
         @mapping = YAML.load_file(path)
       end
 
+      # specify a block to be run for every line in the file. if the block
+      # returns true, reject the line. if false, keep the line.
       def reject_lines(&block)
         @reject_lines = block
       end
 
-      def before_line(&block)
+      # block to be run before each line. only parameter is the line before
+      # mapping in array form.
+      # TODO: figure out if i'm actually going to implement this
+      def before_line(&block) # :nodoc:
         @before_line = block
       end
 
-      def after_line(&block)
+      # block to be run after each line. only parameter is the unmapped line.
+      # TODO: figure out if i'm actually going to implement this
+      def after_line(&block) # :nodoc:
         @after_line = block
       end
 
+      # block to be run once for each top level mapped Item. only parameter is
+      # the item.
       def map(&block)
         @map = block
       end
 
-      def parse(data)
+      # runs a string through the CSV parser.
+      def parse(data) # :nodoc:
         CSV.parse(data)
       end
 
-      def apply_reject_lines(lines)
+      def apply_reject_lines(lines) # :nodoc:
         lines.reject(&@reject_lines)
       end
 
-      def apply_mapping(line, group = nil)
+      # takes a line and our @mapping, returns a hash of the map keys to the
+      # values from the line. +group+ specifies a specific group to map the 
+      # line to. +group+ = nil assumes that the root of @mapping is a valid
+      # group.
+      def apply_mapping(line, group = nil) # :nodoc:
         item = {}
         map_group = mapping
         map_group = map_group[group] unless group.nil?
@@ -57,7 +82,8 @@ module Schlepp
         item
       end
 
-      def line_changed?(line, pieces)
+      # find which group if any has changed from what's in +pieces+.
+      def line_changed?(line, pieces) # :nodoc:
         groups.each_with_index do |key, i|
           current = pieces[i]
           return i if current.nil?
@@ -67,7 +93,9 @@ module Schlepp
         nil
       end
 
-      def build_items(line, group = 0)
+      # return an array of Item objects for each group after +group+ given 
+      # +line+.
+      def build_items(line, group = 0) # :nodoc:
         items = []
         group = -1 if group.nil?
         @mapping[group..-1].each do |item_map|
@@ -80,24 +108,26 @@ module Schlepp
         items
       end
 
-      def apply_groups(data)
+      # this is the most convoluted method in schlepp, haven't decided how i
+      # want to break it up yet.
+      #
+      # data is an array of arrays representing the lines in the file.
+      # #apply_groups takes the data, maps the fields, and nests it based on
+      # groups. It returns an Array of top-level Item objects with nested
+      # +children+.
+      def apply_groups(data) # :nodoc:
         items, pieces, final = [], [], []
         data.each do |line|
           changed = line_changed?(line, pieces)
           new_items = build_items(line, changed)
           changed = groups.size if changed.nil?
 
-          if changed and changed.zero?
-            final << new_items.first
-            items = new_items
-            pieces = []
-            new_items.count.times { pieces << line }
-          elsif
-            items = items.slice(0, changed)
-            items = items + new_items
-            pieces = pieces.slice(0, changed)
-            new_items.count.times { pieces << line }
-          end
+          final << new_items.first if changed.zero?
+
+          items = items.slice(0, changed)
+          items = items + new_items
+          pieces = pieces.slice(0, changed)
+          new_items.count.times { pieces << line }
 
           items[changed..-1].each_with_index do |item,i|
             offset = changed + i - 1
@@ -108,10 +138,16 @@ module Schlepp
         final
       end
 
+      def apply_map(items) # :nodoc:
+        items.each {|i| @map.call(i)}
+      end
+
+      # run #parse, #apply_reject_lines, #apply_grouping, and #apply_map
       def process_file(data)
         data = parse(data)
         data = apply_reject_lines(data) if @reject_lines
-
+        items = apply_grouping(data)
+        apply_map(items) if @map
       end
 
     end
